28ef43e17be3e16f29fd7dbb6a35fa03755a49a8
Cheesymoon <alexsergiuhd@gmail.com>
2025-05-03 21:13:31 +0300

    Adding the start message

--- Easy/RockPaperScissors/main.cpp
+++ Easy/RockPaperScissors/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+
+std::string startingMessage =
+    "Welcome to my Command-Line Rock Paper Scissors game!\n\nBefore the game "
+    "starts, you need to know how the game works.\nWell, you only need to know "
+    "the keys you need to press.\nSo this is all you need to know:\n"
+    "\t- For  Rock press \"1\" or \"R\",\n\t- For  Paper press \"2\" or "
+    "\"P\",\n\t- For  Scissors press \"3\" or \"S\"\n\n\n";
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello, World\n";
+  std::cout << startingMessage;
   return 0;
 }
