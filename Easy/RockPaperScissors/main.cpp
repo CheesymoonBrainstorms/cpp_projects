7eca930f4dd2c0f7bed2539714a7873dd4894b89
Cheesymoon <alexsergiuhd@gmail.com>
2025-05-04 13:01:01 +0300

    Moving functions for readability

--- Easy/RockPaperScissors/main.cpp
+++ Easy/RockPaperScissors/main.cpp
@@ -16,6 +16,32 @@ const string SCISSORS_STR = " Scissors";
 enum Player { USER, BOT };
 enum Choice { ROCK, PAPER, SCISSORS, MISTAKE };
 
+string strToLower(string str);
+void identifyChoice(char c, Choice *usrChoice);
+bool booleanPrompt(string question);
+Player playRound();
+
+string startingMessage =
+    "Welcome to my Command-Line Rock Paper Scissors game!\n\nBefore the game "
+    "starts, you need to know how the game works.\nWell, you only need to know "
+    "the keys you need to press.\nSo this is all you need to know:\n"
+    "\t- For  Rock press \"1\" or \"R\",\n\t- For  Paper press \"2\" or "
+    "\"P\",\n\t- For  Scissors press \"3\" or \"S\"\n\n\n";
+int userScore;
+int botScore;
+
+int main(int argc, char *argv[]) {
+  cout << CLEAR_SCR << startingMessage;
+  if (!booleanPrompt("Ready to begin?")) {
+    return 0;
+  }
+  cout << CLEAR_SCR;
+
+  // playRound();
+
+  return 0;
+}
+
 string strToLower(string str) {
   for (char &c : str) {
     c = tolower(c);
@@ -65,25 +91,4 @@ Player playRound() {
   string userInp;
   getline(cin, userInp);
   identifyChoice(userInp[0], &usrChoice);
-}
-
-string startingMessage =
-    "Welcome to my Command-Line Rock Paper Scissors game!\n\nBefore the game "
-    "starts, you need to know how the game works.\nWell, you only need to know "
-    "the keys you need to press.\nSo this is all you need to know:\n"
-    "\t- For  Rock press \"1\" or \"R\",\n\t- For  Paper press \"2\" or "
-    "\"P\",\n\t- For  Scissors press \"3\" or \"S\"\n\n\n";
-int userScore;
-int botScore;
-
-int main(int argc, char *argv[]) {
-  cout << CLEAR_SCR << startingMessage;
-  if (!booleanPrompt("Ready to begin?")) {
-    return 0;
-  }
-  cout << CLEAR_SCR;
-
-  // playRound();
-
-  return 0;
 }
