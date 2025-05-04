3a3c8260383d14a78b1c05c6a8609206e1f565e3
Cheesymoon <alexsergiuhd@gmail.com>
2025-05-04 12:57:02 +0300

    Creating playRound() function and other related functions

--- Easy/RockPaperScissors/main.cpp
+++ Easy/RockPaperScissors/main.cpp
@@ -10,11 +10,37 @@ using std::cout;
 using std::getline;
 using std::string;
 
+const string ROCK_STR = " Rock";
+const string PAPER_STR = " Paper";
+const string SCISSORS_STR = " Scissors";
+enum Player { USER, BOT };
+enum Choice { ROCK, PAPER, SCISSORS, MISTAKE };
+
 string strToLower(string str) {
   for (char &c : str) {
     c = tolower(c);
   }
   return str;
+}
+
+void identifyChoice(char c, Choice *usrChoice) {
+  switch (tolower(c)) {
+  case '1':
+  case 'a':
+    *usrChoice = ROCK;
+    break;
+  case '2':
+  case 'b':
+    *usrChoice = PAPER;
+    break;
+  case '3':
+  case 'c':
+    *usrChoice = SCISSORS;
+    break;
+  default:
+    *usrChoice = MISTAKE;
+    break;
+  }
 }
 
 bool booleanPrompt(string question) {
@@ -32,12 +58,23 @@ bool booleanPrompt(string question) {
   }
 }
 
+Player playRound() {
+  Choice usrChoice;
+  Choice botChoice;
+  cout << "So, what's it gonna be? [1/2/3]: ";
+  string userInp;
+  getline(cin, userInp);
+  identifyChoice(userInp[0], &usrChoice);
+}
+
 string startingMessage =
     "Welcome to my Command-Line Rock Paper Scissors game!\n\nBefore the game "
     "starts, you need to know how the game works.\nWell, you only need to know "
     "the keys you need to press.\nSo this is all you need to know:\n"
     "\t- For  Rock press \"1\" or \"R\",\n\t- For  Paper press \"2\" or "
     "\"P\",\n\t- For  Scissors press \"3\" or \"S\"\n\n\n";
+int userScore;
+int botScore;
 
 int main(int argc, char *argv[]) {
   cout << CLEAR_SCR << startingMessage;
@@ -45,5 +82,8 @@ int main(int argc, char *argv[]) {
     return 0;
   }
   cout << CLEAR_SCR;
+
+  // playRound();
+
   return 0;
 }
